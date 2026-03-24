a85ac2f0f21c586cab72312e03088173b3950bc3
marco1896-sudo <marco.tepass@gmail.com>
2026-03-24 11:59:01 +0100

    Refactor connectDb to remove in-memory DB option
    Removed in-memory MongoDB server support and related logic.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,21 +1,10 @@
 import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
 
-let memoryServer = null;
-
-export async function connectDb({ mongodbUri, useInMemoryDb = false }) {
+export async function connectDb({ mongodbUri }) {
   mongoose.set('strictQuery', true);
 
-  if (useInMemoryDb) {
-    memoryServer = await MongoMemoryServer.create();
-    const uri = memoryServer.getUri();
-    await mongoose.connect(uri);
-    console.log('[growsim-backend] connected to in-memory MongoDB');
-    return;
-  }
-
   if (!mongodbUri) {
-    throw new Error('MONGODB_URI missing and USE_INMEMORY_DB is not enabled');
+    throw new Error('MONGODB_URI missing');
   }
 
   await mongoose.connect(mongodbUri);
@@ -24,8 +13,4 @@ export async function connectDb({ mongodbUri, useInMemoryDb = false }) {
 
 export async function disconnectDb() {
   await mongoose.disconnect();
-  if (memoryServer) {
-    await memoryServer.stop();
-    memoryServer = null;
-  }
 }
